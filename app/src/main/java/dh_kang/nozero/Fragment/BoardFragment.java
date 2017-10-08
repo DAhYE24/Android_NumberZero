b48290fcf316e51cfb24afa3e4876502a6a3bf27
DAhYE24 <foime1206@swu.ac.kr>
2017-10-08 14:41:30 +0900

    Add widgets left on BoardFragment

--- app/src/main/java/dh_kang/nozero/Fragment/BoardFragment.java
+++ app/src/main/java/dh_kang/nozero/Fragment/BoardFragment.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.h6ah4i.android.widget.advrecyclerview.animator.SwipeDismissItemAnimator;
@@ -21,38 +23,24 @@ import dh_kang.nozero.R;
  * Created by dh93 on 2016-10-14.
  */
 public class BoardFragment extends Fragment {
-    /* 로그 테스트 */
-    private static final String TAG = "NOZERO_FINAL";
+    private static final String TAG = "NOZERO_FINAL";      /* LOG TEST */
 
-    /* XML 선언 */
-    ImageView imgQnA, imgTip;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
-        View v = inflater.inflate( R.layout.frag_board, container, false );
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /* Declare xml components */
+        View view = inflater.inflate(R.layout.frag_board, container, false);
+        RecyclerView list_board;
+        EditText edit_writingEngin;
+        ImageButton btn_searchWriting, btn_write;
 
-//        /* XML 초기화 */
-//        imgQnA = (ImageView)v.findViewById(R.id.fb_imgQnA);
-//        imgTip = (ImageView)v.findViewById(R.id.fb_imgTip);
-//
-//        /* QnA 게시판 */
-//        imgQnA.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i = new Intent(getContext(), acti_ActiBoard.class);
-//                startActivity(i);
-//            }
-//        });
-//
-//        /* TIP 게시판 */
-//        imgTip.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent i = new Intent(getContext(), acti_ActiBoard.class);
-//                startActivity(i);
-//            }
-//        });
+        /* Init widgets*/
+        edit_writingEngin = (EditText) view.findViewById(R.id.edit_writingEngin);
+        list_board = (RecyclerView) view.findViewById(R.id.list_board);
+        btn_searchWriting = (ImageButton) view.findViewById(R.id.btn_searchWriting);
+        btn_write = (ImageButton) view.findViewById(R.id.btn_write);
 
-        RecyclerView list_board = (RecyclerView) v.findViewById(R.id.list_board);
+
+        /* RecyclerView(Custom Library) */
         RecyclerViewSwipeManager swipeManager = new RecyclerViewSwipeManager();
         list_board.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         list_board.setAdapter(swipeManager.createWrappedAdapter(new BoardAdapter()));
@@ -60,6 +48,6 @@ public class BoardFragment extends Fragment {
         swipeManager.attachRecyclerView(list_board);
 //        list_board.addItemDecoration(new SimpleListDividerDecorator(ContextCompat.getDrawable(this, R.drawable.list_divider), true));
 
-        return v;
+        return view;
     }
 }
