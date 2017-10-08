ad2646b18d420e4b60880ba0336a4489f7931888
DAhYE24 <foime1206@swu.ac.kr>
2017-10-08 14:32:33 +0900

    Edit BoardFramgnet.java file

--- app/src/main/java/dh_kang/nozero/Fragment/BoardFragment.java
+++ app/src/main/java/dh_kang/nozero/Fragment/BoardFragment.java
@@ -3,12 +3,18 @@ package dh_kang.nozero.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.h6ah4i.android.widget.advrecyclerview.animator.SwipeDismissItemAnimator;
+import com.h6ah4i.android.widget.advrecyclerview.swipeable.RecyclerViewSwipeManager;
+
 import dh_kang.nozero.Activity.acti_ActiBoard;
+import dh_kang.nozero.Adapter.BoardAdapter;
 import dh_kang.nozero.R;
 
 /**
@@ -24,27 +30,35 @@ public class BoardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
         View v = inflater.inflate( R.layout.frag_board, container, false );
 
-        /* XML 초기화 */
-        imgQnA = (ImageView)v.findViewById(R.id.fb_imgQnA);
-        imgTip = (ImageView)v.findViewById(R.id.fb_imgTip);
+//        /* XML 초기화 */
+//        imgQnA = (ImageView)v.findViewById(R.id.fb_imgQnA);
+//        imgTip = (ImageView)v.findViewById(R.id.fb_imgTip);
+//
+//        /* QnA 게시판 */
+//        imgQnA.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(getContext(), acti_ActiBoard.class);
+//                startActivity(i);
+//            }
+//        });
+//
+//        /* TIP 게시판 */
+//        imgTip.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(getContext(), acti_ActiBoard.class);
+//                startActivity(i);
+//            }
+//        });
 
-        /* QnA 게시판 */
-        imgQnA.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getContext(), acti_ActiBoard.class);
-                startActivity(i);
-            }
-        });
-
-        /* TIP 게시판 */
-        imgTip.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getContext(), acti_ActiBoard.class);
-                startActivity(i);
-            }
-        });
+        RecyclerView list_board = (RecyclerView) v.findViewById(R.id.list_board);
+        RecyclerViewSwipeManager swipeManager = new RecyclerViewSwipeManager();
+        list_board.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        list_board.setAdapter(swipeManager.createWrappedAdapter(new BoardAdapter()));
+        list_board.setItemAnimator(new SwipeDismissItemAnimator());
+        swipeManager.attachRecyclerView(list_board);
+//        list_board.addItemDecoration(new SimpleListDividerDecorator(ContextCompat.getDrawable(this, R.drawable.list_divider), true));
 
         return v;
     }
